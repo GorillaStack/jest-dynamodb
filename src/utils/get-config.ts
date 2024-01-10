e86c5b2cc9694d328c794e7d03facd9c7ce6c669
nicholas-yong <40938341+nicholas-yong@users.noreply.github.com>
2024-01-10 11:34:23 +0800

    test

--- src/utils/get-config.ts
+++ src/utils/get-config.ts
@@ -5,7 +5,8 @@ import type {Config} from '../types';
 export default async function getConfig(debug: any): Promise<Config> {
   const path = process.env.JEST_DYNAMODB_CONFIG || resolve(cwd(), 'jest-dynamodb-config.js');
   const config = require(path);
-  debug('config:', config);
+  debug('TESTING', path);
+  debug('DEBUGGING THE CONFIG', config);
 
   return typeof config === 'function' ? await config() : config;
 }
